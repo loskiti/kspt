2f0467d882ab460400ee1dd88c0d43d4cb725c36
loskiti <loskiti@mail.ru>
2014-12-05 15:28:20 +0300

    задача 6

--- TelBook1.h
+++ TelBook1.h
@@ -15,11 +15,11 @@ public:
 	bool delit(string ); // удаление абонента со всеми номерами
 	vector<string> poiskpoimeni(string);// поиск абонента по имени
 	string poiskpotel(string);// поиск абонента по телефону
-	vector<vector <string>> poiskpobokve(string );// начинающиеся с определенных букв 
+	vector <string> poiskpobokve(string );// начинающиеся с определенных букв 
 	~TelBook();
 	bool dobavlenie( vector <string>&);// добавление абонента с несколькими номерами
 	bool delittel(string, string);// удаление 1 телефона из контакта,
-	                             //у которого телефонов много
+	                               //у которого телефонов много
 	bool OdinNomDobavlenie(string, string); // добавление пары абонент-телефон
 	void vivod ();
 	void vvod();
