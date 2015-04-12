e52ccc85dc20192e6d8baaf2a9e2db09be5616c1
loskiti <loskiti@mail.ru>
2015-04-12 21:41:42 +0300

    Update labyrinth.cpp

--- labyrinth.cpp
+++ labyrinth.cpp
@@ -12,32 +12,32 @@
 #include <string>
 using namespace std;
 // спускаемся на 1 клетку по волне ( с 19 ячейки на 18 и тд)
-void StepDown(int &Out_X, int &Out_Y, int i, vector<vector<int>> &map,string &otvet) 
+void StepDown(int &Out_X, int &Out_Y, int i,int k, vector<vector<int>> &map,string &otvet) 
 { 
 	if (map[Out_Y][Out_X-1]==i-1) 
-	{	
-		map[Out_Y][Out_X]=-3;
+	{	if(k>0)
+			map[Out_Y][Out_X]=-3;
 		Out_X=Out_X-1;
 		otvet.push_back('r');
 		return;
 	}					
 	if (map[Out_Y][Out_X+1]==i-1) 
-	{	
-		map[Out_Y][Out_X]=-3;
+	{	if(k>0)
+			map[Out_Y][Out_X]=-3;
 		Out_X=Out_X+1;
 		otvet.push_back('l');
 		return;
 	}
 	if (map[Out_Y-1][Out_X]==i-1) 
-	{	
-		map[Out_Y][Out_X]=-3;
+	{	if(k>0)
+			map[Out_Y][Out_X]=-3;
 		Out_Y=Out_Y-1;
 		otvet.push_back('d');
 		return;
 	}	
 	if (map[Out_Y+1][Out_X]==i-1) 
-	{	
-		map[Out_Y][Out_X]=-3;
+	{	if(k>0)
+			map[Out_Y][Out_X]=-3;
 		Out_Y=Out_Y+1;
 		otvet.push_back('u');
 		return;
@@ -79,8 +79,8 @@ void StepUp (int &Out_X, int &Out_Y, int i, vector<vector<int>> map, string &otv
 // поиск ячейки с цифрой
 bool Search (int &Out_X,int &Out_Y,int i,int &k,int &summa,int &flag2, vector<vector<int>> &map,string &stroka, vector <int> &stop )
 {
-
-	flag2=0;
+flag2=flag2+1;
+	
 	bool flag;
 	if (k==i)
 	{
@@ -97,19 +97,18 @@ bool Search (int &Out_X,int &Out_Y,int i,int &k,int &summa,int &flag2, vector<ve
 
 				if (i>k) // если ячеек с цифрой на данном отрезке несколько - вернем false и поищем их
 				{
-					StepDown(Out_X, Out_Y, i, map,stroka);
+					StepDown(Out_X, Out_Y, i,k, map,stroka);
 					return true;
 				}
 				else 
 				{
-					StepDown(Out_X, Out_Y, i, map,stroka);
+					StepDown(Out_X, Out_Y, i,k, map,stroka);
 					return false;
 				}
 			}
 		}
 		map[Out_Y][Out_X]=-3; // ячейка нам не подходит
-		flag2=1;
-		StepDown(Out_X, Out_Y, i, map,stroka);
+		StepDown(Out_X, Out_Y, i,k, map,stroka);
 
 		return false;
 	}
@@ -121,8 +120,12 @@ bool Search (int &Out_X,int &Out_Y,int i,int &k,int &summa,int &flag2, vector<ve
 	}
 	if (flag==true) // ячейку нашли, возвращаемся к исходному пути
 	{
-		StepDown(Out_X, Out_Y, i, map,stroka);
-		return true;
+		StepDown(Out_X, Out_Y, i,k, map,stroka);
+		if (i>k)
+		{
+			return true;
+		} 
+		else return false;
 	}
 	else // вдруг по дороге встетится еще одна ячейка с цифрой
 	{
@@ -132,21 +135,20 @@ bool Search (int &Out_X,int &Out_Y,int i,int &k,int &summa,int &flag2, vector<ve
 			flag=Search(Out_X, Out_Y,  i+1, k,summa,flag2, map, stroka, stop);
 			if (flag==true) 
 			{
-				StepDown(Out_X, Out_Y, i, map,stroka);
+				StepDown(Out_X, Out_Y, i,k, map,stroka);
 				return true;
 			}
 			
 		}
 		else
 		{ 
-			if(flag2!=1)
-			{
-				StepDown(Out_X, Out_Y, i, map,stroka);
-			}
+		
+				StepDown(Out_X, Out_Y, i,k, map,stroka);
+			
 			return false;			
 		}
 	}
-	flag2=1;
+	
 	return false;
 }
 
@@ -190,9 +192,9 @@ void wave(int z, vector<vector<int>> &map, int stroka_size, int x)
 }
 int main(int argc,char *argv[])
 {
-	setlocale(LC_ALL, "rus"); 
+	setlocale(LC_ALL, ""); 
 	ifstream file;
-	file.open(argv[1]);
+	file.open("C:\\Users\\Всемогущая Оо\\Downloads\\Tests\\labirint15.txt");
 	char simvol; 
 	vector <vector <int>> map; // поле
 	int i=0;
@@ -200,17 +202,25 @@ int main(int argc,char *argv[])
 	vector <int> stroka;
 	vector <int> stop; // координаты ячейки с цифрой 
 	// чтение поля из файла
+	int max_size=0;
 	while (!(file.eof()))
 	{		
 		file.get(simvol);
-		if (simvol ==10) 
-		{			
+		if (simvol == 10) 
+		{		
+			if(stroka.size()>0)
+			{
 			map.push_back(stroka);
+			if(max_size<stroka.size())
+			max_size=stroka.size();
 			stroka.clear(); 
 			stroka. shrink_to_fit();
 			i++;
 			continue;
+			}
+			else break;
 		};
+		
 
 		switch(simvol) 
 		{
@@ -248,24 +258,32 @@ int main(int argc,char *argv[])
 			}
 		}
 	}
-	stroka.pop_back();
-	map.push_back(stroka);
 	i=0;
-	while (((Entry_X+i)<stroka.size()-1) || ((Entry_X-i)>0))  // распространение волны
-	{
+	if(stroka.size()>0)
+			{
+			map.push_back(stroka);
+			if(max_size<stroka.size())
+				max_size=stroka.size();
+			stroka.clear(); 
+			stroka. shrink_to_fit();
+			}
 
+	 // распространение волны
+for (i=0; max_size*map.size()>i;i++)
+{
 		int n=0,  k=0;
 		while (((Entry_Y+n)>0) || ((Entry_Y+k)<map.size()-1))
 		{
 			if ((Entry_Y+n)>0)
-				wave(Entry_Y+n, map,stroka.size(), Entry_X);
+				wave(Entry_Y+n, map,map[Entry_Y+n].size(), Entry_X);
 			if ((Entry_Y+k)<map.size()-1)
-				wave(Entry_Y+k, map,stroka.size(),Entry_X);
+				wave(Entry_Y+k, map,map[Entry_Y+k].size(),Entry_X);
 			n--;
 			k++;
 		}
-		i++;
+		
 	}
+	
 	i=0;
 	// каждой точке после распространения волны присвивается цифра (кол-во ходов до точки)
 	// сортировка массива по возрастанию  кол-ва шагов до ячейки
@@ -297,52 +315,67 @@ int main(int argc,char *argv[])
 	if(stop.size()>0)  // проверка, есть ли вообще ячейки с цифрами и нужно ли их искать
 		k=map[stop[stop.size()-2]][stop[stop.size()-3]];
 	else  k=-1;
-	int x,y, flag4;
+	int x,y, flag4=0, i1=0;
 	// поиск кротчайшего пути
 	for (i=map[Out_Y][Out_X]; i>-1;i--)
 	{
+		
 		x=Out_X;
 		y=Out_Y;
-		int i1=i;
 		if (k<i)
-			StepDown(Out_X,Out_Y,i,map, otvet);
+			StepDown(Out_X,Out_Y,i,k,map, otvet);
 		if ((k==i)||(k>i)) // цена ячейки равна цене ячейки с цифрой, проверка: она ли это, если нет - поиск
 		{ // точки с этой цифрой
 			if ((stop[stop.size()-3]!=Out_X) ||(Out_Y!=stop[stop.size()-2]))
 			{ // спускаемся вниз, пока не достигнем точки, с которой можно попасть в ячейку с цифрой
+				if(map[Out_Y][Out_X]>0)
+						i=map[Out_Y][Out_X];
 				while ((map[Out_Y][Out_X-1]!=i+1)&&(map[Out_Y][Out_X+1]!=i+1)
 				&&(map[Out_Y+1][Out_X]!=i+1)&&(map[Out_Y-1][Out_X]!=i+1))
 				{
-					StepDown(Out_X,Out_Y,i,map, otvet);
+					if(map[Out_Y][Out_X]>0)
+						i=map[Out_Y][Out_X];
+					if (i==0) 
+					break;
+					StepDown(Out_X,Out_Y,i,k,map, otvet);
 					i--;
 					x=Out_X;
 					y=Out_Y;
+					 i1=i;
 				}
 				string flag3=otvet;
 				flag=Search(Out_X,  Out_Y, i, k,summa,flag4, map, otvet, stop);
 				// если "вес" точки не перекрывает затраченные на нее шаги - мы в эту точку не идем
-				if ((otvet.length()-flag3.length())*0.1>=summa)
+				if (((otvet.length()-flag3.length())*0.1>=summa) && (summa!=0))
+				{
 					otvet=flag3;
-				if (flag4==1)
-				{ // если не были найдены необходимые нам ячейки с цифрами
-					otvet=flag3;
-					Out_X=x;
+					 i=i1+1;
+					 Out_X=x;
 					Out_Y=y;
-					i=map[Out_Y][Out_X]+1;
-
+				}
+							
+				if (summa==0)
+				{
+					otvet.erase(otvet.begin()+flag3.length(), otvet.end()-1);
+					flag4=0;
 				}
 				summa=0;
 				if (flag == true) // если точка найдена, продолжим путь
 				{
 					if ((k==0) && (i==0)) // нашли все ячейки - идем к выходу
 						break;
-					StepDown(Out_X,Out_Y,i,map, otvet);
+					StepDown(Out_X,Out_Y,i,k,map, otvet);
 				} 
 			}
 
 			else 
 			{
-				StepDown(Out_X,Out_Y,i,map, otvet);
+				StepDown(Out_X,Out_Y,i,k,map, otvet);
+				stop.erase(stop.begin() + stop.size()-3, stop.end());
+				if (stop.size()	>0)
+					k=map[stop[stop.size()-2]][stop[stop.size()-3]];
+				else 
+					k=-1;
 			}
 
 		}
