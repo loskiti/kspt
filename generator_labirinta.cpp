0d9f114bf58ef23c7abf32889377c552f4cd6fec
loskiti <loskiti@mail.ru>
2015-04-26 14:32:23 +0300

    Update generator_labirinta.cpp

--- generator_labirinta.cpp
+++ generator_labirinta.cpp
@@ -4,6 +4,7 @@
 #include <fstream>
 # include <vector>
 using namespace std;
+// генерация координат входа и выхода
 void En_Ex(int a, int b,vector<vector<int>> map, int &X, int &Y )
 {
 	int x=0,y=0;
@@ -11,17 +12,17 @@ void En_Ex(int a, int b,vector<vector<int>> map, int &X, int &Y )
 	{
 		x=rand() % a +1;
 		y=rand() % b +1;
-			if (map[y][x]==-2)
-			{
-				x=0;
-				y=0;
-			}
+		if (map[y][x]==-2)
+		{
+			x=0;
+			y=0;
+		}
 	}
-X=x;
-Y=y;
+	X=x;
+	Y=y;
 
 }
-
+// поиск пути по волне
 void StepDown(int &Out_X, int &Out_Y, int i, vector<vector<int>> &map,string &otvet) 
 { 
 	if (map[Out_Y][Out_X-1]==i-1) 
@@ -50,6 +51,7 @@ void StepDown(int &Out_X, int &Out_Y, int i, vector<vector<int>> &map,string &ot
 	}
 
 }
+//распространение волны
 void wave(int z, vector<vector<int>> &map, int stroka_size, int x) 
 {
 	int i=0;
@@ -93,11 +95,11 @@ int main()
 	srand(time(0));
 	int a,b;
 	cout<<"Введите размеры лабиринта:"<<endl;
-		cin>>a>>b;
-	
+	cin>>a>>b;
 	vector <vector <int>> map; 
 	int i;
 	vector <int> stroka;
+	// создание "рамки" лабиринта
 	for(int j=0; j<=b;j++)
 	{
 		for (i=0;i<=a;i++)
@@ -115,9 +117,9 @@ int main()
 		}
 		map.push_back(stroka);
 		stroka.clear(); 
-		//stroka. shrink_to_fit();
+		stroka. shrink_to_fit();
 	}
-
+// генерация 1 строки
 	for (i=1; i<a;i++)
 	{
 		map[1][i]=i;
@@ -134,44 +136,44 @@ int main()
 			i++;
 		}
 	}
-	
-	
-for(int j=2; j<b-1;j++)
+// генерация строк, не считая 1 и последнюю
+
+	for(int j=2; j<b-1;j++)
 	{
-		
+
 		for (i=1;i<a;i++)
 		{
-			
-				if( map[j-1][i]!=-2)
-				{
-					map[j][i]=map[j-1][i];
-				}
-				else 
-				{ 
-					if (i==1)
-					{map[j][i]=1;}
-					else
+
+			if( map[j-1][i]!=-2)
+			{
+				map[j][i]=map[j-1][i];
+			}
+			else 
+			{ 
+				if (i==1)
+				{map[j][i]=1;}
+				else
 					map[j][i]=map[j][i-1];
-				}
+			}
 
 
 		}
 
-		
-for (i=2; i<a-1;i++)
+
+		for (i=2; i<a-1;i++)
 		{
 			if (map[j][i]==map[j][i-1])
 			{
-			if (rand() % 2<1)
-			{
-				map[j][i]=-2;
-				i++;
-			}
+				if (rand() % 2<1)
+				{
+					map[j][i]=-2;
+					i++;
+				}
 			}
 		}
-}
-string otvet;
-
+	}
+	string otvet;
+// генерация последней строки
 	for (i=1;i<a;i++)
 	{
 		if (map[b-1][i]!=-2)
@@ -187,15 +189,16 @@ string otvet;
 			map[b-1][i]=0;
 
 	}
-	
-	
-int Entry_X, Entry_Y=0, Out_X, Out_Y; // координаты входа и выхода
-En_Ex(a-1,b-1,map,Entry_X,Entry_Y);
-map[Entry_Y][Entry_X]=-10;
-En_Ex(a-1,b-1,map,Out_X,Out_Y);
-map[Out_Y][Out_X]=-11;
-cout<<"Сгенерированный лабирин:"<<endl;
-for (i=0;i<b+1;i++)
+
+	int Entry_X, Entry_Y=0, Out_X, Out_Y; // координаты входа и выхода
+	// генерация координат входа и выхода
+	En_Ex(a-1,b-1,map,Entry_X,Entry_Y);
+	map[Entry_Y][Entry_X]=-10;
+	En_Ex(a-1,b-1,map,Out_X,Out_Y);
+	map[Out_Y][Out_X]=-11;
+	cout<<"Сгенерированный лабирин:"<<endl;
+	// вывод лабиринта
+	for (i=0;i<b+1;i++)
 	{
 		for(int j=0; j<a+1;j++)
 		{
@@ -203,21 +206,21 @@ for (i=0;i<b+1;i++)
 			{
 				if (map[i][j]==-10)
 				{	
-				cout<<"*";
-				map[i][j]=0;
-				}
-				else
-			{
-				if (map[i][j]==-11)
-				{	
-				cout<<"e";
-				map[i][j]=-1;
+					cout<<"*";
+					map[i][j]=0;
 				}
 				else
 				{
-				cout<<".";
-				map[i][j]=-1;
-				}
+					if (map[i][j]==-11)
+					{	
+						cout<<"e";
+						map[i][j]=-1;
+					}
+					else
+					{
+						cout<<".";
+						map[i][j]=-1;
+					}
 				}
 			}
 			else
@@ -228,9 +231,10 @@ for (i=0;i<b+1;i++)
 		}
 		cout<<endl;
 	}
-cout<<"(* - вход, е - выход)"<<endl;
-for (i=0; map[1].size()*map.size()>i;i++)
-{
+	cout<<"(* - вход, е - выход)"<<endl;
+	// распространение волны
+	for (i=0; map[1].size()*map.size()>i;i++)
+	{
 		int n=0,  k=0;
 		while (((Entry_Y+n)>0) || ((Entry_Y+k)<map.size()-1))
 		{
@@ -241,21 +245,23 @@ for (i=0; map[1].size()*map.size()>i;i++)
 			n--;
 			k++;
 		}
-		
+
 	}
+	// поиск пути
 	for (i=map[Out_Y][Out_X]; i>-1;i--)
 	{
 		StepDown(Out_X,Out_Y,i,map, otvet);
 	}
 	cout<<"Шаги:"<<endl;
-for (i=otvet.length()-1;i>-1; i--)
+	// вывод пути
+	for (i=otvet.length()-1;i>-1; i--)
 	{
 		cout<<otvet[i];
 	}
-cout<<endl;
-cout<<"d - вниз, u - вверх, l - влево, r - вправо"<<endl;
-map.clear(); 
-	//map. shrink_to_fit();
+	cout<<endl;
+	cout<<"d - вниз, u - вверх, l - влево, r - вправо"<<endl;
+	map.clear(); 
+	map. shrink_to_fit();
 	system ("pause");
 	return 0;
 }
