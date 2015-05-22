a9f8470fa492993dcdae8f13072b1799ce4d5903
loskiti <loskiti@mail.ru>
2015-05-22 22:31:38 +0300

    Update dialog.h

--- KatChat/client/dialog.h
+++ KatChat/client/dialog.h
@@ -3,7 +3,7 @@
 #include <QDialog>
 #include <QTcpSocket>
 #include <QHostAddress>
-#include "C:\QT\2\3\Lab2_server\myclient.h"
+#include "C:\QT\server\myclient.h"
 class  MyClient;
 
 namespace Ui {
